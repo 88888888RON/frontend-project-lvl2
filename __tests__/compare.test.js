c8e2d1bc9394817f72d75632b133b24c3a66d6f0
RIN <ibragimov.rinat@hotmail.com>
2023-05-22 13:41:56 +0300

    after eslint

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,23 +1,25 @@
-import { compare, compareFlatJson, getExtension, objToString } from '../src/compare.js';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import * as fs from 'node:fs';
+import {
+  compare, getExtension,
+} from '../src/compare.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName));
-const expectedFileJson = fs.readFileSync('/home/rin/frontend-project-lvl2/__fixtures__/expectedFile.txt', 'utf-8');
+const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
+const expectedFileJson = readFile('expectedFile.txt');
 
-test("getExtension", () => {
-    expect(getExtension('file.json')).toEqual('json');
-    expect(getExtension('file.yml')).toEqual('yml');
-    expect(getExtension('file.yaml')).toEqual('yaml');
+test('getExtension', () => {
+  expect(getExtension('file.json')).toEqual('json');
+  expect(getExtension('file.yml')).toEqual('yml');
+  expect(getExtension('file.yaml')).toEqual('yaml');
 });
 
-test("expectedFileJson", () => {
-    const fileName1 = `${process.cwd()}/__fixtures__/file1.json`;
-    const fileName2 = `${process.cwd()}/__fixtures__/file2.json`;
+test('expectedFileJson', () => {
+  const fileName1 = `${process.cwd()}/__fixtures__/file1.json`;
+  const fileName2 = `${process.cwd()}/__fixtures__/file2.json`;
 
-    expect(compare(fileName1, fileName2)).toEqual(expectedFileJson);
+  expect(compare(fileName1, fileName2)).toEqual(expectedFileJson);
 });
--- src/compare.js
+++ src/compare.js
@@ -2,47 +2,68 @@ import _ from 'lodash';
 import path from 'node:path';
 import * as fs from 'node:fs';
 
-const compare = (fileName1, fileName2) => {
-    const obj1 = JSON.parse(fs.readFileSync(path.resolve(fileName1)));
-    const obj2 = JSON.parse(fs.readFileSync(path.resolve(fileName2)));
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-    const unionKeys = _.union(keys1, keys2);
-    const sortedUnionKeys = _.sortBy(unionKeys)
-    const resObj = {};
-  
-    for (const key of sortedUnionKeys) {
-      if (!Object.hasOwn(obj2, key)) {
-        resObj[`- ${key}`] = obj1[key];
-      } else if (!Object.hasOwn(obj1, key)) {
-        resObj[`+ ${key}`] = obj2[key];
-      } else if (obj1[key] === obj2[key]) {
-        resObj[`  ${key}`] = obj1[key];
-      } else if (obj1[key] !== obj2[key]) {
-        resObj[`- ${key}`] = obj1[key];
-        resObj[`+ ${key}`] = obj2[key];
-      }
+const getFixturePath = (file) => path.resolve(file);
+const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
+
+const compareFlatJson = (fileName1, fileName2) => {
+  const obj1 = JSON.parse(readFile(fileName1));
+  const obj2 = JSON.parse(readFile(fileName2));
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const unionKeys = _.union(keys1, keys2);
+  const sortedUnionKeys = _.sortBy(unionKeys);
+  const obj = {};
+
+  for (const key of sortedUnionKeys) {
+    if (!Object.hasOwn(obj2, key)) {
+      obj[`- ${key}`] = obj1[key];
+    } else if (!Object.hasOwn(obj1, key)) {
+      obj[`+ ${key}`] = obj2[key];
+    } else if (obj1[key] === obj2[key]) {
+      obj[`  ${key}`] = obj1[key];
+    } else if (obj1[key] !== obj2[key]) {
+      obj[`- ${key}`] = obj1[key];
+      obj[`+ ${key}`] = obj2[key];
     }
-  
-    return resObj;
+  }
+
+  return obj;
 };
 
-const objToString = (fileName1, fileName2) => {
-  const obj = compare(fileName1, fileName2);
-  const strFromObjWOИBrackets = JSON.stringify(obj).slice(1, -1);
-  const arrFromStr = strFromObjWOИBrackets.split(',');
-  const str = arrFromStr
-    .map(el => el.split(':'))
-    .map(([el0, el1]) => {
-      if (el0.startsWith('"')) {
-        el0 = el0.slice(1, -1);
-      };
+const objToString = (obj) => {
+  const str = JSON.stringify(obj)
+    .slice(1, -1) // убираю скобки
+    .split(',') // из строки массив, разделитель - запятая
+    .map((el) => el.split(':')) // каждый элемент массива разделяю на массив, разделитель - двоеточие (массив массивов получился)
+    .map(([el0, el1]) => { // каждый элемент массива (это маленький массив
+      if (el0.startsWith('"')) { // ) распаковываю и отображаю по условию, есть кавычки в начале или нет?
+        el0 = el0.slice(1, -1); // если да, то убираю по символу с начала и конца (кавычки)
+      }
       if (el1.startsWith('"')) {
         el1 = el1.slice(1, -1);
-      };
-      return`${el0}: ${el1}`});
-  const res = `{\n  ${str.join('\n  ')}\n}`
-  return res;
+      }
+      return `${el0}: ${el1}`;
+    }); // собираю ответ
+  const res = `{\n  ${str.join('\n  ')}\n}`; // из массива делаю строку, соединяя элементы переносом строки.
+  return res; // В начало и в конец ставлю { и }, после 1ой делаю перенос строки.
+}; //  Вывод напоминает объект, но им не является.
+
+const getExtension = (file) => {
+  const result = file
+    .split('.')
+    .pop();
+  // console.log(result);
+  return result;
 };
 
-export { objToString };
+const compare = (file1, file2) => {
+  if ((getExtension(file1) === getExtension(file2)) && (getExtension(file1) === 'json')) {
+    // console.log(`равенство выполнилось`);
+    return objToString(compareFlatJson(file1, file2));
+  }
+  return 'error';
+};
+
+export {
+  compare, compareFlatJson, getExtension, objToString,
+};
