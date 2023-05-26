2ac5d1f042cb3084acd7c1abe1ca5196708cd18c
RIN <ibragimov.rinat@hotmail.com>
2023-05-26 10:29:34 +0300

    added parser for json, yml, yaml and txt in to src/parsers.js

---
+++ src/parsers.js
@@ -0,0 +1,18 @@
+import yaml from 'js-yaml';
+
+const parse = (file, ext) => {
+  if (ext === '.json') {
+    console.log(JSON.parse(file));
+    return JSON.parse(file);
+  }
+  if (ext === '.yml' || ext === '.yaml') {
+    console.log(yaml.load(file));
+    return yaml.load(file);
+  }
+  if (file === '.txt') {
+    return file;
+  }
+  return 'unknown format';
+};
+
+export default parse;
