4970a82212db60e0322be61d6ab579e2f6e9bcab
RIN <ibragimov.rinat@hotmail.com>
2023-06-14 14:09:05 +0300

    now it can compare recursive files

--- src/compare.js
+++ src/compare.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import path from 'node:path';
 import * as fs from 'node:fs';
 import parse from './parsers.js';
+import stylish from './formatters/stylish.js';
 
 const getFixturePath = (file) => path.resolve(file);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
@@ -10,59 +11,55 @@ const getExtension = (file) => {
   return ext;
 };
 
-const compareFlatJson = (fileName1, fileName2) => {
+const getDiff = (fileName1, fileName2) => {
   const readFile1 = readFile(fileName1);
   const readFile2 = readFile(fileName2);
   const ext1 = getExtension(fileName1);
   const ext2 = getExtension(fileName2);
   const obj1 = parse(readFile1, ext1);
-  console.log(obj1);
+  // console.log(`obj1 = ${JSON.stringify(obj1, null, ' ')}`);
   const obj2 = parse(readFile2, ext2);
-  console.log(obj2);
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const unionKeys = _.union(keys1, keys2);
-  const sortedUnionKeys = _.sortBy(unionKeys);
+  // console.log(`obj2 = ${JSON.stringify(obj1, null, ' ')}`);
 
-  const object = sortedUnionKeys.reduce((obj, key) => {
-    if (!Object.hasOwn(obj2, key)) {
-      return { ...obj, [`- ${key}`]: obj1[key] };
-    }
-    if (!Object.hasOwn(obj1, key)) {
-      return { ...obj, [`+ ${key}`]: obj2[key] };
-    }
-    if (obj1[key] === obj2[key]) {
-      return { ...obj, [`  ${key}`]: obj1[key] };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return { ...obj, [`- ${key}`]: obj1[key], [`+ ${key}`]: obj2[key] };
-    }
-    return obj;
-  }, {});
+  // eslint-disable-next-line no-shadow
+  const iter = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const unionKeys = _.union(keys1, keys2);
+    const sortedUnionKeys = _.sortBy(unionKeys);
 
-  return object;
+    const res = sortedUnionKeys.reduce((acc, key) => {
+      // eslint-disable-next-line no-prototype-builtins
+      if (!obj1.hasOwnProperty(key)) {
+        return { ...acc, [key]: { type: 'added', value2: obj2[key] } };
+      }
+      // eslint-disable-next-line no-prototype-builtins
+      if (!obj2.hasOwnProperty(key)) {
+        return { ...acc, [key]: { type: 'removed', value1: obj1[key] } };
+      }
+      if (Object.prototype.toString.call(obj1[key]) === '[object Object]' && Object.prototype.toString.call(obj2[key]) === '[object Object]') {
+        return { ...acc, [key]: { type: 'object', children: iter(obj1[key], obj2[key]) } };
+      }
+      if (obj1[key] === obj2[key]) {
+        return { ...acc, [key]: { type: 'unchanged', value1: obj1[key] } };
+      }
+      return { ...acc, [key]: { type: 'changed', value1: obj1[key], value2: obj2[key] } };
+    }, {});
+
+    return res;
+  };
+
+  // console.log(`getDiff(obj1, obj2) = ${JSON.stringify(iter(obj1, obj2), 2, '    ')}`);
+  return iter(obj1, obj2);
 };
 
-const objToString = (obj) => {
-  const str = JSON.stringify(obj)
-    .slice(1, -1) // убираю скобки
-    .split(',') // из строки массив, разделитель - запятая
-    .map((el) => el.split(':')) // каждый элемент массива разделяю на массив, разделитель - двоеточие (массив массивов получился)
-    .map(([el0, el1]) => { // каждый элемент массива (это маленький массив
-      if (el0.startsWith('"')) { // ) распаковываю и отображаю по условию, есть кавычки в начале или нет?
-        el0 = el0.slice(1, -1); // если да, то убираю по символу с начала и конца (кавычки)
-      }
-      if (el1.startsWith('"')) {
-        el1 = el1.slice(1, -1);
-      }
-      return `${el0}: ${el1}`;
-    }); // собираю ответ
-  const res = `{\n  ${str.join('\n  ')}\n}`; // из массива делаю строку, соединяя элементы переносом строки.
-  return res; // В начало и в конец ставлю { и }, после 1ой делаю перенос строки.
-}; //  Вывод напоминает объект, но им не является.
-
-const compare = (file1, file2) => objToString(compareFlatJson(file1, file2));
+const compare = (file1, file2, format) => {
+  if (format === 'stylish') {
+    return stylish(getDiff(file1, file2));
+  }
+  return stylish(getDiff(file1, file2));
+};
 
 export {
-  compare, compareFlatJson, objToString, getExtension,
+  compare, getExtension,
 };
