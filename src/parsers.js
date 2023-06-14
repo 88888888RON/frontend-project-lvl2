97bc25b580761232e857a488cd2e6994d6ec9278
RIN <ibragimov.rinat@hotmail.com>
2023-06-14 14:07:53 +0300

    commited console.log

--- src/parsers.js
+++ src/parsers.js
@@ -2,11 +2,11 @@ import yaml from 'js-yaml';
 
 const parse = (file, ext) => {
   if (ext === '.json') {
-    console.log(JSON.parse(file));
+    // console.log(JSON.parse(file));
     return JSON.parse(file);
   }
   if (ext === '.yml' || ext === '.yaml') {
-    console.log(yaml.load(file));
+    // console.log(yaml.load(file));
     return yaml.load(file);
   }
   if (file === '.txt') {
